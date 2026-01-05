c82cdab945f76a4dc7776aa2a7e5157224d94bb5
nicklearnspython <nicklearnspython@gmail.com>
2026-01-04 21:43:11 -0800

    continued cleanup of clock constants

--- Age_Clock_Main/include/ClockConstants.h
+++ Age_Clock_Main/include/ClockConstants.h
@@ -2,11 +2,13 @@
 
 #include <FastLED.h>
 
-/// Constants
+/// ---------------------------------------------------------------------------
+/// ------------------------------- CONSTANTS ---------------------------------
+/// ---------------------------------------------------------------------------
 
 bool isAutoplayEnabled = true;
 
-// Age Setup
+/// ----------------------------- AGE CONSTANTS -------------------------------
 static const int DAYS_MIN = 0;
 static const int DAYS_MAX = 30;
 static const int MONTHS_MIN = 0;
@@ -29,32 +31,31 @@ static const int BIRTH_YEAR = 2016;
 //static const int BIRTH_MONTH = 6;
 //static const int BIRTH_YEAR = 2021;
 
-// Stepper Constants
-static const int stepperStepsPerRevolution = 2048;
-static const int STEPPER_PIN_1 = 8;
-static const int STEPPER_PIN_2 = 9;
-static const int STEPPER_PIN_3 = 10;
-static const int STEPPER_PIN_4 = 11;
+/// --------------------------- STEPPER CONSTANTS -----------------------------
+static constexpr int STEPPER_PIN_1 = 8;
+static constexpr int STEPPER_PIN_2 = 9;
+static constexpr int STEPPER_PIN_3 = 10;
+static constexpr int STEPPER_PIN_4 = 11;
 
-static const int STEPPER_HOMING_PIN = 5;
-static const int STEPPER_RPM = 10;
+static constexpr int STEPPER_STEPS_PER_REV = 2048;
+static constexpr int STEPPER_RPM = 10;
 
-static const int yearHomePin = 7;
-static const long yearHomeOffsetSteps = 0;
+static constexpr int STEPPER_HOMING_PIN = 5;
+static constexpr long HOMING_OFFSET_STEPS = 0;
 
-// LED Constants
-#define LED_PIN 6
-#define LED_TYPE NEOPIXEL // Previously used WS2812B
+/// ----------------------------- LED CONSTANTS -------------------------------
+static constexpr int LED_PIN = 6;
+#define LED_TYPE NEOPIXEL         // Previously used WS2812B
 #define COLOR_ORDER GRB
 
-static const unsigned int LEDS_PER_DAY = 1;
-static const unsigned int LEDS_PER_MONTH = 2;
-static const unsigned int LEDS_PER_YEAR = 0;
-static const unsigned int DAY_LED_COUNT = (DAYS_MAX + 1) * LEDS_PER_DAY;
-static const unsigned int MONTH_LED_COUNT = (MONTHS_MAX + 1) * LEDS_PER_MONTH;
-static const unsigned int YEAR_LED_COUNT = (YEARS_MAX + 1) * LEDS_PER_YEAR;
+static constexpr unsigned int LEDS_PER_DAY = 1;
+static constexpr unsigned int LEDS_PER_MONTH = 2;
+static constexpr unsigned int LEDS_PER_YEAR = 0;
+static constexpr unsigned int DAY_LED_COUNT = (DAYS_MAX + 1) * LEDS_PER_DAY;
+static constexpr unsigned int MONTH_LED_COUNT = (MONTHS_MAX + 1) * LEDS_PER_MONTH;
+static constexpr unsigned int YEAR_LED_COUNT = (YEARS_MAX + 1) * LEDS_PER_YEAR;
 
-// Servo Constants
+/// ---------------------------- SERVO CONSTANTS ------------------------------
 static const int dayServoPin = 11;
 static const int monthServoPin = 10;
 static const int yearServoPin = 9;
@@ -70,7 +71,7 @@ static const uint8_t dayAngles[] = {0, 5, 9, 13, 20, 26, 31, 36, 41, 47, 51, 56,
 static const uint8_t monthAngles[] = {0, 15, 30, 44, 58, 72, 85, 100, 117, 133, 150, 170};
 static const uint8_t yearAngles[] = {0, 8, 18, 27, 37, 47, 57, 68, 80, 92, 104, 117, 130, 141, 154, 166, 179};
 
-// GPS Constants
+/// ----------------------------- GPS CONSTANTS -------------------------------
 static const uint8_t RXPin = 4, TXPin = 3;
 static const uint8_t dummy_RXPin = 13, dummy_TXPin = 12;
 static const uint32_t GPSBaud = 9600;
